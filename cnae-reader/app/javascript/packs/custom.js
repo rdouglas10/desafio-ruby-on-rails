8ede815a5e2cf101024aa57fd4b76b1717d94b7a
randerson <randerson_d@hotmail.com>
2020-07-07 23:11:02 -0300

    Ajustes nos aquivos para deploy e coreção no js

--- cnae-reader/app/javascript/packs/custom.js
+++ cnae-reader/app/javascript/packs/custom.js
@@ -1,7 +1,11 @@
 $("#send-cnab").click(function () {
   var delay = 2000; //2 seconds
   setTimeout(function () {
-    alert("Dados Inseridos com sucesso!");
+    if ($("#archive").val() != "") {
+      alert("Dados Inseridos com sucesso!");
+    } else {
+      alert("Erro no envio");
+    }
     location.reload().delay(2000); //your code to be executed after 2 seconds
   }, delay);
 });
